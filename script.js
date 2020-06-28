55cfd3868bcc44fa14a3979399f44df802e24ee5
Kostya Osipenko <osipenkokonstantin91@gmail.com>
2020-06-28 14:56:00 +0300

    home work

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
-// let num = Math.floor(Math.random() * 10);
-// let guess = + prompt('number');
-//  if (num === guess) {
-// console.log('you win')
-//  } else  if ( num > guess){
-//      console.log('number highter')
-//  } else if (num < guess){
-//      console.log('number lower')
-//  } else {
-//      console.log('try again')
-//  }
-//  console.log('game over');
+// exercise one
+ let userAge = + prompt ('Enter your age');
+   if (isNaN(NaN) === Number.isNaN(userAge)) {
+      console.log('You enter string');
+   } else if (userAge >= 18){
+      console.log('Welcome to the jungle');
+   } else if (userAge <= 12) {
+      console.log('you are so young, go play on the yard');
+   }
 
-let typeOfAction = prompt(' enter action type');
-let CREATE_ACTION = 'update';
-let READ_ACTION = 'read';
-let UPADATE_ACTION = 'creat';
-let DELETE_ACTION = 'delete';
-switch (typeOfAction) {
-   case CREATE_ACTION:
-       console.log('CREAT')
-       break;  
-       case READ_ACTION:
-           console.log('READ');
-           break;
-           default:
-               console.log('Deafault behavior');
-}
+//  exercise two
+ (isNaN(NaN) === Number.isNaN(userAge)) ? console.log('You enter string') :
+ (userAge >= 18) ? console.log('you rock') :
+ (userAge <=12) ? console.log('Ha-ha,go play...anywhere ')  : '' ;
 
+let candidateAge = + prompt('Enter your Age');
+let workExp = + prompt('Enter your work Exp');
+let driveLicense = prompt('do you have driver license.Enter "yes" or "not"');
+let pets = prompt('Do you have some pets.Enter "yes" or "not"');
+let married = prompt('Do you married.Enter "yes" or "not"');
+let gender = prompt('Indicate you gender');
+let haveChild = prompt('Do you have a child.Enter "yes" or "not"');
+ if (candidateAge >= 30 && workExp >= 3 && pets == 'yes' && driveLicense == 'yes' && gender =='male' && married =='yes' && haveChild == 'yes') {
+   console.log('Exellent.You are accepted')
+ } else {
+   console.log('Sorry, you dont suit us')
+ }
